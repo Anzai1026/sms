04768cd45c9f026fe158d05acb14eb81143f6830
Anzai1026 <uchu.anzai1026@gmail.com>
2024-06-25 10:26:37 +0900

    message functions

--- lib/firestore/room_firestore.dart
+++ lib/firestore/room_firestore.dart
@@ -6,40 +6,48 @@ import 'package:sms/utils/shared_prefs.dart';
 
 class RoomFirestore {
   static final FirebaseFirestore _firebaseFirestoreInstance = FirebaseFirestore.instance;
-  static final _roomCollection = _firebaseFirestoreInstance.collection('room');
+  static final CollectionReference _roomCollection = _firebaseFirestoreInstance.collection('room');
   static final joinedRoomSnapshot = _roomCollection.where('joined_user_ids', arrayContains: SharedPrefs.fetchUid()).snapshots();
 
-  static Future<void> createRoom(String myUid) async{
+  // メッセージ削除のメソッド
+  static Future<void> deleteMessage(String roomId, String messageId) async {
+    try {
+      await _roomCollection.doc(roomId).collection('message').doc(messageId).delete();
+    } catch (e) {
+      print('メッセージの削除失敗 ===== $e');
+    }
+  }
+
+  static Future<void> createRoom(String myUid) async {
     try {
       final docs = await UserFirestore.fetchUsers();
-      if(docs == null) return;
-      docs.forEach((doc) async{
-        if(doc.id == myUid) return;
+      if (docs == null) return;
+      for (var doc in docs) {
+        if (doc.id == myUid) continue;
         await _roomCollection.add({
           'joined_user_ids': [doc.id, myUid],
           'created_time': Timestamp.now()
         });
-      });
-    } catch(e) {
+      }
+    } catch (e) {
       print('ルーム作成失敗 ===== $e');
     }
-
   }
 
-  static Future<List<TalkRoom>?> fetchJoinedRooms(QuerySnapshot snapshot) async{
+  static Future<List<TalkRoom>?> fetchJoinedRooms(QuerySnapshot snapshot) async {
     try {
       String myUid = SharedPrefs.fetchUid()!;
       List<TalkRoom> talkRooms = [];
-      for(var doc in snapshot.docs) {
+      for (var doc in snapshot.docs) {
         Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
         List<dynamic> userIds = data['joined_user_ids'];
         late String talkUserUid;
-        for(var id in userIds) {
-          if(id == myUid) continue;
+        for (var id in userIds) {
+          if (id == myUid) continue;
           talkUserUid = id;
         }
         User? talkUser = await UserFirestore.fetchProfile(talkUserUid);
-        if(talkUser == null) return null;
+        if (talkUser == null) return null;
         final talkRoom = TalkRoom(
             roomId: doc.id,
             talkUser: talkUser,
@@ -47,34 +55,33 @@ class RoomFirestore {
         );
         talkRooms.add(talkRoom);
       }
-      print(talkRooms.length);
-
       return talkRooms;
-    } catch(e) {
+    } catch (e) {
       print('参加してるルームの取得失敗 ----- $e');
       return null;
     }
   }
+
   static Stream<QuerySnapshot> fetchMessageSnapshot(String roomId) {
     return _roomCollection.doc(roomId).collection('message').orderBy('send_time', descending: true).snapshots();
   }
 
-  static Future<void> sendMessage({required String roomId, required String message}) async{
+  static Future<void> sendMessage({required String roomId, required String message, String replyToMessage = ''}) async {
     try {
       final messageCollection = _roomCollection.doc(roomId).collection('message');
       await messageCollection.add({
-        'message' : message,
-        'sender_id' : SharedPrefs.fetchUid(),
-        'send_time' : Timestamp.now()
+        'message': message,
+        'sender_id': SharedPrefs.fetchUid(),
+        'send_time': Timestamp.now(),
+        'reply_to_message': replyToMessage,
       });
 
       await _roomCollection.doc(roomId).update({
         'last_message': message
       });
 
-    } catch(e) {
+    } catch (e) {
       print('メッセージの送信失敗 ===== $e');
     }
   }
-
 }
--- lib/pages/talk_room_page.dart
+++ lib/pages/talk_room_page.dart
@@ -6,6 +6,9 @@ import 'package:intl/intl.dart' as intl;
 import 'package:sms/model/talk_room.dart';
 import 'package:sms/utils/shared_prefs.dart';
 
+import '../firestore/user_firestore.dart';
+import '../model/user.dart';
+
 class TalkRoomPage extends StatefulWidget {
   final TalkRoom talkRoom;
   const TalkRoomPage(this.talkRoom, {Key? key}) : super(key: key);
@@ -16,8 +19,42 @@ class TalkRoomPage extends StatefulWidget {
 
 class _TalkRoomPageState extends State<TalkRoomPage> {
   final TextEditingController controller = TextEditingController();
+  String? recipientProfileImageUrl;
+  Message? replyMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchRecipientProfileImage();
+  }
+
+  Future<void> fetchRecipientProfileImage() async {
+    String? recipientUid = widget.talkRoom.talkUser.uid;
+    if (recipientUid != null) {
+      User? recipientUser = await UserFirestore.fetchProfile(recipientUid);
+      if (recipientUser != null) {
+        setState(() {
+          recipientProfileImageUrl = recipientUser.imagePath;
+        });
+      }
+    }
+  }
+
+  String formatDate(DateTime date) {
+    return intl.DateFormat('yyyy-MM-dd').format(date);
+  }
+
+  bool isNewDay(DateTime current, DateTime previous) {
+    return formatDate(current) != formatDate(previous);
+  }
+
+  Future<void> deleteMessage(String messageId) async {
+    await RoomFirestore.deleteMessage(widget.talkRoom.roomId, messageId);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final ColorScheme colors = Theme.of(context).colorScheme;
     return Scaffold(
       backgroundColor: Colors.white70,
       appBar: AppBar(
@@ -28,81 +65,218 @@ class _TalkRoomPageState extends State<TalkRoomPage> {
           StreamBuilder<QuerySnapshot>(
             stream: RoomFirestore.fetchMessageSnapshot(widget.talkRoom.roomId),
             builder: (context, snapshot) {
-              if(snapshot.hasData) {
+              if (snapshot.hasData) {
                 return Padding(
                   padding: const EdgeInsets.only(bottom: 60.0),
                   child: ListView.builder(
-                      physics: const RangeMaintainingScrollPhysics(),
-                      shrinkWrap: true,
-                      reverse: true,
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        final doc = snapshot.data!.docs[index];
-                        final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-                        final Message message = Message(
-                            message: data['message'],
-                            isMe: SharedPrefs.getUid() == data['sender_id'],
-                            sendTime: data['send_time']
-                        );
-                        return Padding(
-                          padding: EdgeInsets.only(top: 10.0, left: 10, right: 10, bottom: index == 0 ? 10 : 0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            textDirection: message.isMe ? TextDirection.rtl : TextDirection.ltr,
-                            children: [
-                              Container(
-                                  constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.7),
-                                  decoration: BoxDecoration(
-                                    color: message.isMe ? Colors.greenAccent : Colors.white,
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
-                                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                                  child: Text(message.message)
-                              ),
-                              Text(intl.DateFormat('HH.mm').format(message.sendTime.toDate()),style: const TextStyle(fontWeight: FontWeight.w500))
-                            ],
-                          ),
-                        );
+                    physics: const RangeMaintainingScrollPhysics(),
+                    shrinkWrap: true,
+                    reverse: true,
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      final doc = snapshot.data!.docs[index];
+                      final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+                      final Message message = Message(
+                        id: doc.id,
+                        message: data['message'],
+                        isMe: SharedPrefs.fetchUid() == data['sender_id'],
+                        sendTime: data['send_time'],
+                        replyToMessage: data['reply_to_message'] ?? '',
+                      );
+
+                      final bool isRecipientMessage = widget.talkRoom.talkUser.uid == data['sender_id'];
+
+                      final DateTime currentMessageDate = message.sendTime.toDate();
+                      bool showDateHeader = false;
+                      if (index == snapshot.data!.docs.length - 1) {
+                        showDateHeader = true;
+                      } else {
+                        final previousDoc = snapshot.data!.docs[index + 1];
+                        final Map<String, dynamic> previousData = previousDoc.data() as Map<String, dynamic>;
+                        final DateTime previousMessageDate = (previousData['send_time'] as Timestamp).toDate();
+                        showDateHeader = isNewDay(currentMessageDate, previousMessageDate);
                       }
+
+                      return Column(
+                        children: [
+                          if (showDateHeader)
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10.0),
+                              child: Text(
+                                intl.DateFormat('yyyy-MM-dd').format(currentMessageDate),
+                                style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
+                              ),
+                            ),
+                          GestureDetector(
+                            onLongPress: () {
+                              showModalBottomSheet(
+                                context: context,
+                                builder: (context) {
+                                  return Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: ListTile(
+                                          leading: const Icon(Icons.reply),
+                                          title: const Text('Reply'),
+                                          onTap: () {
+                                            setState(() {
+                                              replyMessage = message;
+                                            });
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                      ),
+                                      if (message.isMe)
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: ListTile(
+                                            leading: const Icon(Icons.delete),
+                                            title: const Text('Delete'),
+                                            onTap: () {
+                                              deleteMessage(message.id);
+                                              Navigator.pop(context);
+                                            },
+                                          ),
+                                        ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                top: 10.0,
+                                left: 10,
+                                right: 10,
+                                bottom: index == 0 ? 10 : 0,
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                textDirection: message.isMe ? TextDirection.rtl : TextDirection.ltr,
+                                children: [
+                                  if (!message.isMe && recipientProfileImageUrl != null)
+                                    CircleAvatar(
+                                      backgroundImage: NetworkImage(recipientProfileImageUrl!),
+                                      radius: 15,
+                                    ),
+                                  Container(
+                                    constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.7),
+                                    decoration: BoxDecoration(
+                                      color: message.isMe ? Colors.greenAccent : Colors.white,
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        if (message.replyToMessage.isNotEmpty)
+                                          Container(
+                                            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 3),
+                                            margin: const EdgeInsets.only(bottom: 5),
+                                            decoration: BoxDecoration(
+                                              color: Colors.grey.shade200,
+                                              borderRadius: BorderRadius.circular(5),
+                                            ),
+                                            child: Text(
+                                              message.replyToMessage,
+                                              style: const TextStyle(color: Colors.black54, fontSize: 12),
+                                            ),
+                                          ),
+                                        Text(message.message),
+                                      ],
+                                    ),
+                                  ),
+                                  Text(
+                                    intl.DateFormat('HH.mm').format(message.sendTime.toDate()),
+                                    style: const TextStyle(fontWeight: FontWeight.w500),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
                   ),
                 );
               } else {
-                return const Center(child: Text('メッセージがありません'),);
+                return const Center(
+                  child: Text('メッセージがありません'),
+                );
               }
-            }
+            },
           ),
           Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
+              if (replyMessage != null)
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                  color: Colors.grey.shade200,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Replying to: ${replyMessage!.message}',
+                          style: const TextStyle(color: Colors.black54),
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.close),
+                        onPressed: () {
+                          setState(() {
+                            replyMessage = null;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
               Container(
                 color: Colors.white,
                 height: 60,
                 child: Row(
                   children: [
-                    Expanded(child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: TextField(
-                        controller: controller,
-                        decoration: const InputDecoration(
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextField(
+                          controller: controller,
+                          decoration: const InputDecoration(
                             hintText: 'Type a message',
-                          contentPadding: EdgeInsets.only(left: 15),
-                          border: OutlineInputBorder()
+                            contentPadding: EdgeInsets.only(left: 15),
+                            border: OutlineInputBorder(),
+                          ),
                         ),
                       ),
-                    )),
+                    ),
                     IconButton(
-                        onPressed: () async{
-                          await RoomFirestore.sendMessage(
-                              roomId: widget.talkRoom.roomId,
-                              message: controller.text
-                          );
-                          controller.clear();
-                        },
-                        icon: const Icon(
-                          Icons.send_rounded,
-                          color: Colors.greenAccent,
-                          size: 40.0,
-                        ))
+                      onPressed: () async {
+                        await RoomFirestore.sendMessage(
+                          roomId: widget.talkRoom.roomId,
+                          message: controller.text,
+                          replyToMessage: replyMessage?.message ?? '',
+                        );
+                        controller.clear();
+                        setState(() {
+                          replyMessage = null;
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.send_rounded,
+                        size: 40.0,
+                      ),
+                      style: IconButton.styleFrom(
+                        foregroundColor: colors.onPrimary,
+                        backgroundColor: colors.primary,
+                        disabledBackgroundColor: colors.onSurface.withOpacity(0.12),
+                        hoverColor: colors.onPrimary.withOpacity(0.08),
+                        focusColor: colors.onPrimary.withOpacity(0.12),
+                        highlightColor: colors.onPrimary.withOpacity(0.12),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -111,7 +285,7 @@ class _TalkRoomPageState extends State<TalkRoomPage> {
                 height: MediaQuery.of(context).padding.bottom,
               ),
             ],
-          )
+          ),
         ],
       ),
     );
